bd00beb2f1a4a2f8a8ce9b84ac95351c06e5cdb1
anegm98 <anegm98.eg@gmail.com>
2020-10-19 11:15:22 +0200

    fix dump and add load

--- protencoder/cli.py
+++ protencoder/cli.py
@@ -10,14 +10,21 @@ def main():
     parser.add_argument('_', nargs='*')
     parser.add_argument("-d", "--seqPath", required=True,
                         help="file path of the protein sequences")
+    parser.add_argument("-m", "--maxLen", default="2000",
+                        help="maximum length of the protein to be encoded;\
+                        default = 2000")
+    parser.add_argument("-o", "--outPrefix", default="",
+                        help="output files prefix")
     args = parser.parse_args()
 
     seqPath = args.seqPath
+    maxLen = int(args.maxLen)
+    outPrefix = args.outPrefix if args.outPrefix != "" else seqPath[:-6]
 
-    oneHotencd = ONEencoder()
+    oneHotencd = ONEencoder(maxLen)
     oneHotencd.handler.read_fasta(seqPath)
     oneHotencd.encode()
-    oneHotencd.handler.dump(seqPath[:-4])
+    oneHotencd.handler.dump(outPrefix)
     return 0
 
 
