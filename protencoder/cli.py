3953f38b7a77ac79a010089317183025866e0434
anegm98 <anegm98.eg@gmail.com>
2020-10-25 02:17:42 +0200

    fix errors in loading files

--- protencoder/cli.py
+++ protencoder/cli.py
@@ -2,8 +2,55 @@
 import argparse
 import sys
 import glob
+from Bio import SeqIO
+from collections import Counter
 from protencoder.onehot import ONEencoder
 from protencoder.GOencoder import GOencoder
+
+
+def devide(seqPath, chopSize, outPrefix):
+    fasta_sequences = SeqIO.parse(seqPath, 'fasta')
+    x = 0
+    newFasta = list()
+    part = 1
+    for seq in fasta_sequences:
+        newFasta.append(seq)
+        x += 1
+        if x == chopSize:
+            SeqIO.write(newFasta, outPrefix+"_part"+str(part)+".fasta",
+                        'fasta')
+            newFasta = list()
+            x = 0
+            part += 1
+    SeqIO.write(newFasta, outPrefix+"_part"+str(part)+".fasta",
+                'fasta')
+
+
+def create_filter(collection, numFreqGO, outPrefix):
+    seqDict = {'F': list(), 'P': list(), 'C': list()}
+    GOAf = open(collection)
+    for line in GOAf.readlines():
+        line = line.rstrip("\n").split("\t")
+        seqDict[line[2]].append(line[1])
+    GOAf.close()
+    ofile = outPrefix+"_filter_"+str(numFreqGO)
+    filterF = open(ofile, 'w')
+    counter = Counter(seqDict['F'])
+    most_occur = [x[0] for x in counter.most_common(numFreqGO)]
+    for GOA in most_occur:
+        filterF.write(GOA+"\t"+'F'+"\n")
+
+    counter = Counter(seqDict['P'])
+    most_occur = [x[0] for x in counter.most_common(numFreqGO)]
+    for GOA in most_occur:
+        filterF.write(GOA+"\t"+'P'+"\n")
+
+    counter = Counter(seqDict['C'])
+    most_occur = [x[0] for x in counter.most_common(numFreqGO)]
+    for GOA in most_occur:
+        filterF.write(GOA+"\t"+'C'+"\n")
+    filterF.close()
+    return ofile
 
 
 def main():
@@ -22,7 +69,7 @@ def main():
     parser.add_argument("-m", "--maxLen", default="2000",
                         help="maximum length of the protein to be encoded;\
                         if -1 m = max protein length, default = 2000")
-    parser.add_argument("-p", "--GOpartioned", default="2000",
+    parser.add_argument("-p", "--GOpartioned", default="0",
                         help="if annotated keys are divided;\
                         0(default) if False, 1 if True")
     parser.add_argument("-o", "--outPrefix", default="",
@@ -30,10 +77,13 @@ def main():
     parser.add_argument("-s", "--chopSize", default="-1",
                         help="number of sequences to be loaded\
                         into memory per time")
-    parser.add_argument("-F", "--GOfilter", default="", help="file path of\
-                        GO keys to be used for encoding filteration")
-    parser.add_argument("-c", "--collection", default="", help="file path of\
-                        collected protein annotations")
+    GOfilterParser = parser.add_mutually_exclusive_group()
+    GOfilterParser.add_argument("-F", "--GOfilter", default="",
+                                help="file path of GO keys to be used for\
+                                encoding filteration")
+    GOfilterParser.add_argument("-c", "--collection", default="",
+                                help="file path of collected\
+                                protein annotations")
     parser.add_argument("-n", "--numFreqGO", default="500", help="number of\
                         the most frequent GO annotions to be considered")
     args = parser.parse_args()
@@ -49,35 +99,10 @@ def main():
     numFreqGO = int(args.numFreqGO)
     outPrefix = args.outPrefix
 
-    if collection != "":
-        from collections import Counter
-
-        seqDict = {'F': list(), 'P': list(), 'C': list()}
-        GOAf = open(collection)
-        for line in GOAf.readlines():
-            line = line.rstrip("\n").split("\t")
-            seqDict[line[2]].append(line[1])
-        GOAf.close()
-
-        filterF = open(outPrefix+"_filter_"+str(numFreqGO), 'w')
-        counter = Counter(seqDict['F'])
-        most_occur = [x[0] for x in counter.most_common(numFreqGO)]
-        for GOA in most_occur:
-            filterF.write(GOA+"\t"+'F'+"\n")
-
-        counter = Counter(seqDict['P'])
-        most_occur = [x[0] for x in counter.most_common(numFreqGO)]
-        for GOA in most_occur:
-            filterF.write(GOA+"\t"+'P'+"\n")
-
-        counter = Counter(seqDict['C'])
-        most_occur = [x[0] for x in counter.most_common(numFreqGO)]
-        for GOA in most_occur:
-            filterF.write(GOA+"\t"+'C'+"\n")
-
-        filterF.close()
     if not (seqPath is None):
         outPrefix = args.outPrefix if args.outPrefix != "" else seqPath[:-6]
+        if collection != "":
+            create_filter(collection, numFreqGO, outPrefix)
         oneHotencd = ONEencoder(maxLen)
         if Protfilter != "":
             oneHotencd.load_filter(Protfilter)
@@ -86,32 +111,17 @@ def main():
             oneHotencd.encode()
             oneHotencd.dump(outPrefix)
         else:
-            from Bio import SeqIO
-            fasta_sequences = SeqIO.parse(seqPath, 'fasta')
-            x = 0
-            newFasta = list()
-            part = 1
-            for seq in fasta_sequences:
-                newFasta.append(seq)
-                x += 1
-                if x == chopSize:
-                    SeqIO.write(newFasta, outPrefix+"_part"+str(part)+".fasta",
-                                'fasta')
-                    newFasta = list()
-                    x = 0
-                    part += 1
-            SeqIO.write(newFasta, outPrefix+"_part"+str(part)+".fasta",
-                        'fasta')
-            x = 1
-            for name in glob.glob(outPrefix+"_part*"):
+            devide(seqPath, chopSize, outPrefix)
+            for name in glob.glob(outPrefix+"_part*.fasta"):
                 oneHotencd.read(name)
                 oneHotencd.encode()
-                oneHotencd.dump(outPrefix+"_part"+str(x))
-                x += 1
+                num = name[name.find("part") + 4:-6]
+                oneHotencd.dump(outPrefix+"_part"+num)
 
     elif not (GOfile is None):
         outPrefix = args.outPrefix if args.outPrefix != "" else GOfile[:-4]
-
+        if collection != "":
+            GOfilter = create_filter(collection, numFreqGO, outPrefix)
         if not GOpartioned:
             oneHotencd = GOencoder()
             if Protfilter != "":
@@ -120,14 +130,13 @@ def main():
             oneHotencd.encode()
             oneHotencd.dump(outPrefix)
         else:
-            x = 1
             for name in glob.glob(Protfilter):
                 oneHotencd = GOencoder()
                 oneHotencd.load_filter(name, GOfilter)
                 oneHotencd.read(GOfile)
                 oneHotencd.encode()
-                oneHotencd.dump(outPrefix+"_part"+str(x))
-                x += 1
+                num = name[name.find("part") + 4:-6]
+                oneHotencd.dump(outPrefix+"_part"+num)
     return 0
 
 
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/anegm98/protencoder',
-    version='1.1.0',
+    version='1.1.1',
     zip_safe=False,
     package_data={'protencoder': ['data/*']}
 )
